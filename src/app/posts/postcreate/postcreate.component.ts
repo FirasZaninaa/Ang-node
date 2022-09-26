f498bfa7cf16c6cb2ff117925047c2951c726fe7
Firas Zanina <firaszanina22@gmail.com>
2022-09-26 16:58:53 +0200

    final commit

--- src/app/posts/postcreate/postcreate.component.ts
+++ src/app/posts/postcreate/postcreate.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-postcreate',
   templateUrl: './postcreate.component.html',
   styleUrls: ['./postcreate.component.css']
 })
-export class PostcreateComponent implements OnInit {
-  newPost = '';
-  onAddPost(){
-    this.newPost = 'The user is me';
-  }
+export class PostcreateComponent  {
+  enteredTitle = "";
+  enteredContent = "";
+  @Output() postCreated = new EventEmitter<Post>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content
+    };
+    this.postCreated.emit(post);
   }
 
 }
